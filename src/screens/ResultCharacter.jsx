6b83362ae738fc1c5e70a7a223a6d839f8d978da
juyeonBae <w2316@e-mirim.hs.kr>
2025-03-20 12:11:21 +0900

    📝docs : 캐릭터 4개 삭제

--- src/screens/ResultCharacter.jsx
+++ src/screens/ResultCharacter.jsx
@@ -5,31 +5,27 @@ import { DRContext } from "../context/DRContext"
 
 import hermione from "../assets/images/Hermione.png"
 import herrypoter from "../assets/images/harryPotter.png"
-import ron from "../assets/images/ron.png"
-import nevil from "../assets/images/nevil.png"
+import ron from "../assets/images/ron.png" 
 
 import runa from "../assets/images/runa.png"
 import cho from "../assets/images/cho.png"
-import philius from "../assets/images/philius.png"
-import helena from "../assets/images/helena.png"
+import philius from "../assets/images/philius.png" 
 
 import pomona from "../assets/images/pomona.png"
-import sedric from "../assets/images/sedric.png"
-import nimpadora from "../assets/images/nimpadora.png"
+import sedric from "../assets/images/sedric.png" 
 import hellga from "../assets/images/hellGa.png"
 
 import malpoy from "../assets/images/malpoy.png"
-import snape from "../assets/images/snape.png"
-import slughorn from "../assets/images/slughorn.png"
+import snape from "../assets/images/snape.png" 
 import voldemort from "../assets/images/voldemort.png"
 
 import goHome from "../assets/images/goHomeB.png"
 
 const characterImages = [
-    hermione, herrypoter, ron, nevil,
-    runa, cho, philius, helena,
-    pomona, sedric, nimpadora, hellga,
-    malpoy, snape, slughorn, voldemort
+    hermione, herrypoter, ron,
+    runa, cho, philius,
+    pomona, sedric, hellga,
+    malpoy, snape, voldemort
 ]
 
 export default function ResultCharacter() {
